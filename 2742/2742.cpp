e721d210f6c4b6846e2584b5c75a53cfb584e232
jungskim <dostwoman@gmail.com>
2023-04-25 14:08:09 +0900

    update 230425

---
+++ 2742/2742.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int range = 0;
+	
+	scanf("%d", &range);
+
+	for (int i = 1; i <= range; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
