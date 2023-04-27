53f1e9593ab4e31d80d372243e5bacabaf42c7c5
jungskim <dostwoman@gmail.com>
2023-04-27 11:07:33 +0900

    2744

---
+++ 2744/2744/2744.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char str[100];
+	int i = -1;
+
+	scanf("%s", str);
+	while(str[++i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	printf("%s", str);
+
+	return (0);
+}
