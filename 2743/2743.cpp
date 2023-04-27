343b707fa46a2d1fa789daa803336550cea44c31
jungskim <dostwoman@gmail.com>
2023-04-27 10:58:43 +0900

    2743

---
+++ 2743/2743.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char str[100] = "";
+	int len = 0;
+
+	scanf("%s", str);
+	while (str[len])
+		len++;
+	printf("%d", len);
+
+	return (0);
+}
