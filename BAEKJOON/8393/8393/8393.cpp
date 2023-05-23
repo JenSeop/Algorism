cd1e2d444ae1c41a2e5c09bbd5cb32e51fd257d1
jungskim <dostwoman@gmail.com>
2023-05-23 21:00:26 +0900

    8393

---
+++ BAEKJOON/8393/8393/8393.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int range = 0;
+	int summary = 0;
+	int index = -1;
+
+	scanf("%d", &range);
+	while (index != range)
+		summary = summary + (++index);
+	printf("%d", summary);
+
+	return (0);
+}
